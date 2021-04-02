820fecdc9bf78297335eea10ad693efaad5f07cb
Subham Panigrahi <subhamp.official@gmail.com>
2021-04-02 09:58:52 +0530

    Navbar Extraction

--- inventory/views.py
+++ inventory/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from .models import Item, Category
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'inventory/home.html')
+    all_items = Item.objects.all()
+    context = {
+        'all_items': all_items
+    }
+    return render(request, 'inventory/home.html', context)
 
 
 def about(request):
