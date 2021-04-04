ac1c0f6aff58ba028a6edf14d0fabfc472e7cf3d
PKSingh0017 <priyanshusingh1998@gmail.com>
2021-04-04 16:16:13 +0530

    added customers in admin page

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Item, OrderItem, Order, Category, Payment
+from .models import Item, OrderItem, Order, Category, Payment, Customer
 
 admin.site.register(Item)
 admin.site.register(OrderItem)
 admin.site.register(Order)
 admin.site.register(Category)
 admin.site.register(Payment)
+admin.site.register(Customer)
